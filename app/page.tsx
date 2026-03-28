444ce9709c2725e49255899bbf6fbcbdc31f1c70
Anca110 <ancutailenei55@gmail.com>
2026-03-28 20:38:22 +0200

    Anca

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/sections/Hero";
 import HowItWorks from "@/components/sections/HowItWorks";
+import Roles from "@/components/sections/Roles";
+
+
 
 
 export default function Home() {
@@ -9,6 +12,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <HowItWorks />
+      <Roles/>
     </main>
   );
 }
