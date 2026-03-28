3d9aaa8776fabe8531f9a0483a818e31b4a4d8c7
aileniisalome <salomeailenii33@gmail.com>
2026-03-28 21:58:17 +0200

    WhatItDoes Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/sections/Hero";
 import HowItWorks from "@/components/sections/HowItWorks";
+import WhatItDoes from "@/components/sections/WhatItDoes";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <HowItWorks />
+      <WhatItDoes />
     </main>
   );
 }
