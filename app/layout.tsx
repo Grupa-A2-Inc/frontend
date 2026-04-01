29950a304f170189708d839a019a1ee3400f237c
TilicaMihail <mihailtilica1@gmail.com>
2026-04-01 17:11:57 +0300

    fixed all the bullshit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,20 @@
 import "./globals.css";
 import StoreProvider from "@/store/StoreProvider";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;700;800;900&family=Fira+Code:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" rel="stylesheet" />
       </head>
       <body className="bg-brand-bg text-brand-text antialiased">
         <StoreProvider>
           <ThemeProvider>
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
+            {children}
           </ThemeProvider>
-         </StoreProvider>
+        </StoreProvider>
       </body>
     </html>
   );
