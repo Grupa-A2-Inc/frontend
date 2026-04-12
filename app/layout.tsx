9b26588ebd310fdccaa081d335bf3b4b9627b548
Bianca <bia.bianca.curmei@gmail.com>
2026-04-13 02:55:35 +0300

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import StoreProvider from "@/store/StoreProvider";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import AutoLogin from "./AutoLogin";
+import Navbar from "@/components/layout/Navbar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <StoreProvider>
           <AutoLogin />
           <ThemeProvider>
+            <Navbar />
             {children}
           </ThemeProvider>
         </StoreProvider>
